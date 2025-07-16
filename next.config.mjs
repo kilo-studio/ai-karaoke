2c0bac7e4a7191f4e1807b2800f199822d9cc461
grillinmuffins <griffin@kilo.studio>
2025-07-15 20:03:12 -0400

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true
+  },
+  env: {
+    GENIUS_ACCESS_TOKEN: process.env.GENIUS_ACCESS_TOKEN,
+    OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+  }
+};
 
 export default nextConfig;
